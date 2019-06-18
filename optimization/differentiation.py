4a15c9d373265dff893185ea02d3998c7903cffa
royael <plevints@gmail.com>
2019-06-18 16:04:56 +0200

    frac diff

---
+++ optimization/differentiation.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+
+def frac_diff(series, d, thres=.01):
+    """
+    Increasing width window, with treatment of NaNs
+    Note 1: For thres = 1, nothing is skipped.
+    Note 2: d can be any positive fractional, not necessarily bounded [0,1].
+    """
+    #1) Compute weights for the longest series
+    w = get_weights(d, series.shape[0])
+    #2) Determine initial calcs to be skipped based on weight-loss threshold
+    w_ = np.cumsum(abs(w))
+    w_ /= w_[-1]
+    skip = w_[w_ > thres].shape[0]
+    #3) Apply weights to values
+    df = {}
+    for name in series.columns:
+        seriesF,df_ = series[[name]].fillna(method='ffill').dropna(), pd.Series()
+        for iloc in range(skip, seriesF.shape[0]):
+            loc = seriesF.index[iloc]
+            if not np.isfinite(series.loc[loc, name]):
+                continue  # exclude NAs
+            df_[loc] = np.dot(w[-(iloc+1):, :].T, seriesF.loc[:loc])[0, 0]
+        df[name] = df_.copy(deep=True)
+    df = pd.concat(df, axis=1)
+    return df
+
+
+def frac_diff_ffd(series, d, thres=1e-5):
+    """
+    Constant width window
+    Note 1: thres determines the cut-off weight for the window
+    Note 2: d can be any positive fractional, not necessarily bounded [0,1].
+    """
+    #1) Compute weights for the longest series
+    w = get_weights(d, series.shape[0], thres)
+    width = len(w) - 1
+    #2) Apply weights to values
+    df = {}
+    for name in series.columns:
+        seriesF, df_ = series[[name]].fillna(method='ffill').dropna(), pd.Series()
+        for iloc1 in range(width, seriesF.shape[0]):
+            loc0, loc1 = seriesF.index[iloc1-width], seriesF.index[iloc1]
+            if not np.isfinite(series.loc[loc1, name]):
+                continue  # exclude NAs
+            df_[loc1] = np.dot(w.T, seriesF.loc[loc0:loc1])[0, 0]
+        df[name] = df_.copy(deep=True)
+    df = pd.concat(df, axis=1)
+    nan_df = pd.DataFrame(np.nan, index=series.index[:width], columns=df.columns)
+    return pd.concat([nan_df, df], axis=0)
+
+
+def plotMinFFD(series):
+    out = pd.DataFrame(columns=['adfStat', 'pVal', 'lags', 'nObs', '95% conf', 'corr'])
+    for d in np.linspace(0, 1, 11):
+#        df1 = np.log(series).resample('1D').last()  # downcast to daily obs
+        df1 = np.log(series)
+        df2 = frac_diff_ffd(df1, d, thres=.01).dropna()
+        corr = np.corrcoef(df1.loc[df2.index].iloc[:, 0], df2.iloc[:, 0])[0, 1]
+        df2 = adfuller(df2.iloc[:, 0], maxlag=1, regression='c', autolag=None)
+        out.loc[d] = list(df2[:4]) + [df2[4]['5%']] + [corr]  # with critical value
+    out[['adfStat', 'corr']].plot(secondary_y='adfStat')
+    plt.axhline(out['95% conf'].mean(), linewidth=1, color='r', linestyle='dotted')
+    plt.show()
+    return out
+
+
+def get_weights(d, size, h=0.):
+    # thres > 0 drops insignificant weights
+    w = [1.]
+    for k in range(1, size):
+        w_ = -w[-1]/k*(d-k+1)
+        w.append(w_)
+    w = np.array(w[::-1])
+    w = w[np.abs(w) > h].reshape(-1, 1)
+    return w
