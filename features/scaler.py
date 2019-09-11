1ca4162b2b3117d14eb00276b85358c109f8812b
royael <plevints@gmail.com>
2019-09-11 14:27:33 +0700

    add scaling functions

--- features/scaler.py
+++ features/scaler.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def minmax_scaler(data, split=0.55):
-    return (data.T/np.max(data[:int(data.shape[0]*split)], axis=0)).T
+    return data/np.max(data[:int(data.shape[0]*split)], axis=0)
 
 def standard_scaler(data, split=0.55):
     return (data-np.mean(data[:int(data.shape[0]*split)], axis=0))/np.std(data[:int(data.shape[0]*split)], axis=0)
