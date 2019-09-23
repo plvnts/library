eaf178faace9a041b34e82e1c46bd5c02ec03946
royael <plevints@gmail.com>
2019-09-23 17:33:18 +0700

    add sigmoid scaler

--- features/normalization.py
+++ features/normalization.py
@@ -5,3 +5,6 @@ def minmax_scaler(data, split=0.55):
 
 def standard_scaler(data, split=0.55):
     return (data-np.mean(data[:int(data.shape[0]*split)], axis=0))/np.std(data[:int(data.shape[0]*split)], axis=0)
+
+def sigmoid_scaler(data):
+    return 1/(1+np.exp(-data))
